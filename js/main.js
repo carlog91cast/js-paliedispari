9d9652c4cf15d3eb544ad43dbd6703ba85af5126
bigeppo <carlog91cast@gmail.com>
2022-06-22 15:40:38 +0200

    pari e dispari: add math random

--- js/main.js
+++ js/main.js
@@ -2,3 +2,10 @@
 
 // l'utente sceglie pari o dispari e inserisce un numero da 1 a 5
 
+const userNum = prompt('inserisci un numero');
+
+// ora stabilisco per il pc un numero randomico da 1 a 5 utilizzando la funzione
+
+function getRandomNum(6) {
+    Math.floor((Math.random() * 6) + 1);
+} 
