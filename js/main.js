e36e0dbe23c6861ac165633b69d382d94d47a811
bigeppo <carlog91cast@gmail.com>
2022-06-22 16:49:47 +0200

    add prompt pari o dispari

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri 
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
+
+
+// Chiedo Pari o dispari
+
+    const inputPairDispair = prompt("Pari o dispari?");
+
+ while (!(inputPairDispair == "pari" || inputEvenOdd == "dispari"))
+
+console.log("Hai inserito: " + inputPairDispair);
