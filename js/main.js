f0314002593b9d40555855d57c6b53c0b73dc824
bigeppo <carlog91cast@gmail.com>
2022-06-22 18:21:25 +0200

    intro palindromi

--- js/main.js
+++ js/main.js
@@ -1,59 +1,62 @@
-// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-// Sommiamo i due numeri 
-// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
-// Dichiariamo chi ha vinto.
+// // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// // Sommiamo i due numeri 
+// // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// // Dichiariamo chi ha vinto.
 
 
-// Chiedo Pari o dispari
+// // Chiedo Pari o dispari
 
-const inputPairDispair = prompt("Pari o dispari?");
-console.log("Hai inserito: " + inputPairDispair);
+// const inputPairDispair = prompt("Pari o dispari?");
+// console.log("Hai inserito: " + inputPairDispair);
 
-while (!(inputPairDispair == "pari" || inputPairDispair == "dispari")) {
-    inputPairDispair++;
-}
+// while (!(inputPairDispair == "pari" || inputPairDispair == "dispari")) {
+//     inputPairDispair++;
+// }
 
-// chiedo un numero da 1 a 5
+// // chiedo un numero da 1 a 5
 
-let inputNum = 0;
-inputNum = parseInt(prompt("Inserisci un numero da 1 a 5"));
+// let inputNum = 0;
+// inputNum = parseInt(prompt("Inserisci un numero da 1 a 5"));
 
-while (inputNum < 1 || inputNum >= 5 || isNaN(inputNum)) {
-    inputNum++;
-};
+// while (inputNum < 1 || inputNum >= 5 || isNaN(inputNum)) {
+//     inputNum++;
+// };
 
-console.log("Hai inserito: " + inputNum);
+// console.log("Hai inserito: " + inputNum);
 
-// genero un numero randomico per il pc usando una funzione
+// // genero un numero randomico per il pc usando una funzione
 
-function randomNum() {
-    return Math.floor(Math.random() * (5 - 1 + 1) + 1);
-}
-const pcNum = randomNum(1, 5);
+// function randomNum() {
+//     return Math.floor(Math.random() * (5 - 1 + 1) + 1);
+// }
+// const pcNum = randomNum(1, 5);
 
-console.log("pc inserisce: " + pcNum);
+// console.log("pc inserisce: " + pcNum);
 
-// sommo i due numeri
+// // sommo i due numeri
 
-let sum = inputNum + pcNum;
+// let sum = inputNum + pcNum;
 
-console.log("la somma è: " + sum);
+// console.log("la somma è: " + sum);
 
-function pairDispair(num) {
-    if (num % 2 === 0) {
-        return true;
-    }
-    return false;
-};
+// function pairDispair(num) {
+//     if (num % 2 === 0) {
+//         return true;
+//     }
+//     return false;
+// };
 
-// stabiliamo chi è il vincitore
+// // stabiliamo chi è il vincitore
 
-if (pairDispair(sum) && inputPairDispair == "pari" || !pairDispair(num) && imputiPairDispair == "dispari") {
-    console.log("vince il giocatore")
-} else{
-    console.log("vince il computer");
-}
+// if (pairDispair(sum) && inputPairDispair == "pari" || !pairDispair(num) && imputiPairDispair == "dispari") {
+//     console.log("vince il giocatore")
+// } else{
+//     console.log("vince il computer");
+// }
+
+// palindromi
+
 
 
 
